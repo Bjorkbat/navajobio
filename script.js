010278bac6911a6e2d0b74212da67dd92e8ba90c
aliciamb83 <dazhidesign18@gmail.com>
2016-11-15 12:37:25 -0700

    Added location pinpoints

---
+++ script.js
@@ -0,0 +1,7 @@
+var url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBt6bSMG9O5zn4_4XJqszwDQnbCl0lhjxk"
+
+// &callback=initMap
+// "http://AIzaSyBt6bSMG9O5zn4_4XJqszwDQnbCl0lhjxk"
+
+// Place a draggable marker on the map
+;
